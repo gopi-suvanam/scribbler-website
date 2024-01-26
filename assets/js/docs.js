3fd089c43e1f4ac1520f2ceec020a312f6294106
Gopi Krishna Suvanam <68820170+gopi-suvanam@users.noreply.github.com>
2024-01-26 23:01:49 +0530

    Update docs.js

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -28,22 +28,24 @@ $(function() {
     });
 
     // Make the triangular pattern in the header
-    if (uiColors) {
-        const $masthead = $('.site-masthead');
-
-        if ($masthead.length) {
-            const t = new Trianglify({
-                cellsize: 90,
-                noiseIntensity: 0,
-                x_gradient: [
-                    uiColors[0],
-                    uiColors[1],
-                ],
-            });
-            const pattern = t.generate(window.screen.width | $masthead.outerWidth(), $masthead.outerHeight() * 1.2);
-
-            const style = $('<style>.site-masthead { background-image: ' + pattern.dataUrl + '; }</style>');
-            $('html > head').append(style);
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        if (uiColors) {
+            const $masthead = $('.site-masthead');
+    
+            if ($masthead.length) {
+                const t = new Trianglify({
+                    cellsize: 90,
+                    noiseIntensity: 0,
+                    x_gradient: [
+                        uiColors[0],
+                        uiColors[1],
+                    ],
+                });
+                const pattern = t.generate(window.screen.width | $masthead.outerWidth(), $masthead.outerHeight() * 1.2);
+    
+                const style = $('<style>.site-masthead { background-image: ' + pattern.dataUrl + '; }</style>');
+                $('html > head').append(style);
+            }
         }
     }
 });
